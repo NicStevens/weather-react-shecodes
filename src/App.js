74f1c70ed42242e6f8e606bcc318b365e57532ab
Nicky Stevens <lnickystevens@gmail.com>
2024-08-20 21:05:25 -0400

    Added statement and GitHub project link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+      <p>
+        This app was coded by Nicky Stevens and is open-sourced on{" "}
+        <a
+          href="https://github.com/NicStevens/weather-react-shecodes"
+          rel="noreferrer"
+          target="_blank"
+        >
+          GitHub
+        </a>
+        .{" "}
+      </p>
     </div>
   );
 }
